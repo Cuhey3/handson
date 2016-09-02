23f503855e542bde1f23d65d4279671782236a3c
doi-shuhei <doi@nagisalabs.org>
2016-09-02 11:24:11 +0900

    first commit

---
+++ src/main/java/com/mycode/handson/App.java
@@ -0,0 +1,8 @@
+package com.mycode.handson;
+
+public class App {
+
+    public static void main(String[] args) {
+        Fruits fruits = new Fruits();
+    }
+}
---
+++ src/main/java/com/mycode/handson/Fruits.java
@@ -0,0 +1,5 @@
+package com.mycode.handson;
+
+public class Fruits {
+
+}
